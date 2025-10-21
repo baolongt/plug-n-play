149cde244c150a99316d9161c856d882f6e1f9af
Long Tran <57053685+baolongt@users.noreply.github.com>
2025-10-21 13:46:17 +0700

    update AdapterInterface

--- src/types/AdapterTypes.ts
+++ src/types/AdapterTypes.ts
@@ -1,3 +1,6 @@
+import { Signer } from "@slide-computer/signer";
+import { SignerAgent } from "@slide-computer/signer-agent";
+
 export interface AdapterConstructor {
   new (config: any): AdapterInterface;
 }
@@ -7,7 +10,7 @@ export interface AdapterConfig {
   enabled: boolean;
   logo: string;
   walletName: string;
-  chain: 'ICP' | 'SOL' | 'ETH' | 'MULTI';
+  chain: "ICP" | "SOL" | "ETH" | "MULTI";
   website?: string;
   adapter: AdapterConstructor;
   config: {
@@ -28,19 +31,19 @@ export interface GetActorOptions {
 }
 
 export enum AdapterStatus {
-  INIT = 'INIT',
-  READY = 'READY',
-  CONNECTING = 'CONNECTING',
-  CONNECTED = 'CONNECTED',
-  DISCONNECTING = 'DISCONNECTING',
-  DISCONNECTED = 'DISCONNECTED',
-  ERROR = 'ERROR',
+  INIT = "INIT",
+  READY = "READY",
+  CONNECTING = "CONNECTING",
+  CONNECTED = "CONNECTED",
+  DISCONNECTING = "DISCONNECTING",
+  DISCONNECTED = "DISCONNECTED",
+  ERROR = "ERROR",
 }
 
 export enum AdapterChain {
-  ICP = 'icp',
-  SOL = 'sol',
-  ETH = 'eth',
+  ICP = "icp",
+  SOL = "sol",
+  ETH = "eth",
 }
 
 export interface AdapterAddresses {
@@ -55,10 +58,28 @@ export interface AdapterInterface {
   getPrincipal(): Promise<string>;
   getAccountId(): Promise<string>;
   getAddresses(): Promise<AdapterAddresses>;
-  createActor<T>(canisterId: string, idl: any, options?: { requiresSigning?: boolean }): any;
-  undelegatedActor?<T>(canisterId: string, idlFactory: any, options?: { requiresSigning?: boolean }): any;
+  createActor<T>(
+    canisterId: string,
+    idl: any,
+    options?: { requiresSigning?: boolean }
+  ): any;
+  undelegatedActor?<T>(
+    canisterId: string,
+    idlFactory: any,
+    options?: { requiresSigning?: boolean }
+  ): any;
+
+  /**
+   * Get current Signer instance, only exist if signer implement BaseSignerAdapter
+   */
+  getSigner(): Signer | null;
+
+  /**
+   * Get current SignerAgent instance, only exist if signer implement BaseSignerAdapter
+   */
+  getSignerAgent(): SignerAgent<Signer> | null;
 }
 
 export interface AdapterWallet {
   adapter: AdapterConstructor;
-} 
+}
